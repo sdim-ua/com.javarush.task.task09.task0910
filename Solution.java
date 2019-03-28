7852758580a4e68df6c8397fa50080f164ee525a
sdim-ua <ingersol@yandex.ua>
2019-03-28 22:29:12 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+     
+      try{
+        //напишите тут ваш код
+
+        ArrayList<String> list = new ArrayList<String>();
+        String s = list.get(18);
+
+        //напишите тут ваш код
+      }
+        catch(IndexOutOfBoundsException e){
+          System.out.println(e.getClass());
+            }
+       
+       
+        
+    }
